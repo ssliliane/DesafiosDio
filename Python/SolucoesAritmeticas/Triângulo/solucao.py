3de55873e4d85b31137a377a7e491a0e6d5c1999
Liliane <lilianes@kyros.com.br>
2021-08-19 19:12:40 -0300

    Inclusao dos desafios em Python

---
+++ Python/SolucoesAritmeticas/Triângulo/solucao.py
@@ -0,0 +1,31 @@
+'''
+############################################################################################################
+############################################## TRIANGULO ###################################################
+############################################################################################################
+
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. 
+Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+
+
+Perimetro = XX.X
+
+
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+
+
+Area = XX.X
+
+Entrada
+A entrada contém três valores reais.
+
+Saída
+O resultado deve ser apresentado com uma casa decimal.
+
+'''
+a = [float(x) for x in input().split()]
+
+if a[0] + a[1 ] > a[2] and a[ 0 ] + a[2] > a[ 1 ] and a[1] + a[ 2  ] > a[ 0 ]:  
+    print(f"Perimetro = {sum(a):.1f}")
+else:
+    area = ((a[0] + a[1]) * a[2]) / 2
+    print("Area = {}".format(area))
