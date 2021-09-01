523b096eb36da73c6b8f9c474a772bf4a37cc6b4
Liliane <lilianes@kyros.com.br>
2021-09-01 12:58:58 -0300

    Inclusao de novos desafios Python

---
+++ Python/DesafiosEmPython/OMaior/solucao.py
@@ -0,0 +1,29 @@
+'''
+############################################################################################################
+################################################ O MAIOR ###################################################
+############################################################################################################
+
+Desafio:
+Desenvolva um programa capaz de ler três valores e apresentar o maior deles e adicionar a mensagem “ eh o maior”. 
+Use a seguinte forma como base:
+
+MaiorAB = (a + b + abs(a-b))/2
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). 
+Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada:
+O arquivo de entrada contém três valores inteiros.
+
+Saída:
+Imprima o maior dos três valores seguido pela mensagem " eh o maior".
+
+'''
+
+import math
+a, b ,c= map(eval, input().split())
+
+maior1 = (a + b + abs(a-b))/2
+maior2 = (maior1 + c + abs(maior1-c))/2
+
+print("%i eh o maior" %maior2)
